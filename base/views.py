dd2a9467b93a9e2f8bd10f78885c1012636d49c5
WyneZ <wynekyawhan@gmail.com>
2023-10-11 02:38:28 +0630

    done v2-integrated custom user model(upload images)

--- base/views.py
+++ base/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib import messages  # For Flash messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 
 # Create your views here.
@@ -25,29 +25,29 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == "POST":
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, 'User Not Found')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, 'Username or Password does not Exist')
+            messages.error(request, 'Email or Password does not Exist')
 
     context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -79,8 +79,9 @@ def userProfile(request, pk):
 def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
+
     if request.method == "POST":
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
--- dj_7hrs_1/settings.py
+++ dj_7hrs_1/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "corsheaders",  # အခြားနေရာက api data ခေါ်သုံးလို့ရအောင် python -m pip install django-cors-headers
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
@@ -116,9 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
+MEDIA_URL = "/images/"
+
+# for css and js file
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# user uploaded picture
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT =
 
